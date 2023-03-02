f790e77ba33214b21255b7910512c1429b38c233
Mohammodullah Emran <mhoemran@gmail.com>
2023-03-02 13:45:36 +0600

    new commit

---
+++ Online_Book_Store_Application_Web/CustomMiddleWare.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Online_Book_Store_Application_Web
+{
+    public class CustomMiddleWare : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Response.Headers.Remove("X-Powered-By");
+            context.HttpContext.Response.Headers.Remove("Server");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Do nothing.
+        }
+    }
+}
